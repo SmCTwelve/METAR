58b9ac9da488f3b8aa0fd2fa719be5780f450617
SmCTwelve <smctwelve@gmail.com>
2017-08-13 14:30:51 +0100

    Add value logging

--- src/metar.py
+++ src/metar.py
@@ -11,12 +11,25 @@ Options:
 -r --raw   Output raw METAR data.
 '''
 
+import logging
 from docopt import docopt
 
-# Get arguments and options as dict
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s')
+
+# Get arguments and options from CLI as dict
 args = docopt(__doc__)
 
 # Store values
-icao = args['<ICAO>']
+icao = args['<ICAO>'].upper()
 time = [args['<time>'], args['<timeEnd>']]
 raw = args['--raw']
+
+logging.debug('Given ICAO is %s and raw ouput is %s' % (icao, raw))
+
+# Check if a single time, time range, or no times given
+if None not in time:
+    logging.debug('A time range was provided: {}'.format(time))
+elif time[0] is not None:
+    logging.debug('A single time was given: {}'.format(time[0]))
+else:
+    logging.debug('No time given: {}'.format(time))
